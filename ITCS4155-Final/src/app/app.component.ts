41d9d1d92b524e21a78a10aadb10d940ab9c5fe8
Christian <acostacastanonchristian@gmail.com>
2023-02-17 11:50:21 -0500

    ignore: test out angular and git repo

--- ITCS4155-Final/src/app/app.component.ts
+++ ITCS4155-Final/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import {pets} from "./pets";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ITCS4155-Final';
+  pets = pets;
+  title = 'Furrble';
 }
---
+++ ITCS4155-Final/src/app/pets.ts
@@ -0,0 +1,21 @@
+export interface Pets{
+  id: number;
+  name: string;
+  animalType: string;
+  breed: string;
+}
+
+export const pets = [
+  {
+    id: 1,
+    name: "Bossman",
+    animalType: "dog",
+    breed: "mixed terrier"
+  },
+  {
+    id: 2,
+    name: "Betty",
+    animalType: "cat",
+    breed: "idk"
+  }
+]
