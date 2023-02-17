b427234dca89e4392145a51ec82c37a714226097
Santiago Dubon <santiagodubon@outlook.com>
2023-02-17 17:11:22 -0500

    Created Angular Components

--- ITCS4155-Final/src/app/app.module.ts
+++ ITCS4155-Final/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
