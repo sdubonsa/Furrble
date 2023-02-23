6419c37436063c2466109a6aca6aa30a9e88bceb
Santiago Dubon <santiagodubon@outlook.com>
2023-02-23 13:20:16 -0500

    Setup routes for register page and homepage

--- ITCS4155-Final/src/app/app-routing.module.ts
+++ ITCS4155-Final/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterPageComponent },
+  { path: '', component: HomepageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [RegisterPageComponent, HomepageComponent];
