78f4ec4e54e3d22f9f9b3a6cde260b45aaab2397
Daniel Floyd <shortstack40floyd@gmail.com>
2023-02-23 19:43:23 -0500

    Adding Password encryption

--- ITCS4155-Final/src/app/register-page/register-page.component.ts
+++ ITCS4155-Final/src/app/register-page/register-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as bcrypt from 'bcryptjs';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
@@ -8,3 +8,19 @@ import { Component } from '@angular/core';
 export class RegisterPageComponent {
 
 }
+
+const saltRounds = 10;  //  Data processing speed
+var password = "456";  // Original Password
+var password2 = "456";
+bcrypt.hash(password, saltRounds, function(err, hash) { // Salt + Hash
+  bcrypt.compare(password2, hash, function(err, result) {  // Compare
+    // if passwords match
+    if (result) {
+          console.log("It matches!")
+    }
+    // if passwords do not match
+    else {
+          console.log("Invalid password!");
+    }
+  });
+});
